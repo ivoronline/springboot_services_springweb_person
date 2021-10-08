af226d7128b8115e0525204229d3e71ed4b27936
igoreta <ivoronline@gmail.com>
2021-10-08 14:02:33 +0200

    Initial commit

---
+++ src/main/java/com/ivoronline/springboot_services_springweb_person/SpringbootServicesSpringwebPersonApplication.java
@@ -0,0 +1,13 @@
+package com.ivoronline.springboot_services_springweb_person;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootServicesSpringwebPersonApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringbootServicesSpringwebPersonApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/ivoronline/springboot_services_springweb_person/SpringbootServicesSpringwebPersonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ivoronline.springboot_services_springweb_person;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootServicesSpringwebPersonApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
